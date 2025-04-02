71edb9c1e4a813042332a20a66ad4597fc26a6d3
skuullking <146060891+skuullking@users.noreply.github.com>
2025-04-02 13:27:12 +0200

    .

--- src/test/java/com/example/DevSecOps/DevSecOpsApplicationTests.java
+++ src/test/java/com/example/DevSecOps/DevSecOpsApplicationTests.java
@@ -9,7 +9,7 @@ class DevSecOpsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Car car = new Car("AA11BB", "audi", 2000);
+		Car car = new Car("AA11BB", "ferrari", 2000);
 		String plate = car.getPlateNumber();
 
 		assertEquals("AA11BB", plate);
